220b4def6193f2ad17799ab2bf2710fe934776d2
Ricardo Alves <ricardospalves@gmail.com>
2019-01-12 02:50:46 -0200

    Usando as diretivas condicionais do VueJS

---
+++ condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  new Vue({
+    el: '#desafio',
+    data: {
+      array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+      livro: {
+        titulo: 'O Senhor dos Anéis',
+        autor: 'J.R.R. Tolkiens',
+        volume: '3'
+      },
+      aluno: {
+        id: 10,
+        nome: 'Maria',
+        notas: [7.67, 8.33, 6.98, 9.21]
+      },
+      isVisible: true,
+      teams: ['Palmeiras', 'Flamengo', 'Cruzeiro', 'Grêmio']
+    }
+  });
+})();
