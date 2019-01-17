10d5b7188a8c674116347792ae63dd7f2050f0ec
Ricardo Alves <ricardospalves@gmail.com>
2019-01-17 01:05:05 -0200

    Mudança geral no Vue e na estrutura HTML

--- projetos/1-matador-de-monstro/app.js
+++ projetos/1-matador-de-monstro/app.js
@@ -1,11 +1,60 @@
 (function() {
   'use strict';
 
+  class Player {
+    constructor({id, title}) {
+      this.id = id;
+      this.title = title;
+      this.life = 100;
+      this.range = {
+        min: 0,
+        max: 100
+      };
+    }
+
+    decrement(value) {
+      const result = this.life - value;
+
+      if(result > 0) {
+        this.life -= value;
+      }
+
+      else {
+        this.life = this.range.min;
+      }
+    }
+
+    increment(value) {
+      const result = this.life + value;
+
+      if(result < 100) {
+        this.life += value;
+      }
+
+      else {
+        this.life = this.range.min;
+      }
+    }
+  }
+
   const app = new Vue({
     el: '#app',
     data: {
       isPlaying: false,
-      showMessage: false,
+      players: [
+        new Player({
+          id: 'you',
+          title: 'Você'
+        }),
+        new Player({
+          id: 'monster',
+          title: 'Monstro'
+        })
+      ],
+      result: {
+        isShow: false,
+        message: ''
+      },
       btnClasses: [
         'p-2',
         'm-2',
@@ -19,57 +68,39 @@
           'my-2',
           'rounded'
         ]
-      },
-      players: [
-        {
-          id: 'player',
-          title: 'Jogador',
-          life: 100
-        },
-        {
-          id: 'monster',
-          title: 'Monstro',
-          life: 100
-        }
-      ]
+      }
     },
     methods: {
-      giveUp() {
-        this.isPlaying = false;
-        this.logs.messages = [];
+      start() {
+        this.isPlaying = true
+        this.result.isShow = false
       },
-      play() {
-        this.isPlaying = true;
-
-        this.players.forEach(player => {
-          player.life = 100;
-        });
+      giveUp() {
+        this.isPlaying = false
       },
       getPlayerById(id) {
-        return this.players.filter(item => item.id === id)[0]
-      },
+        return this.players.filter(item => item.id === id)[0];
+      }
     },
     computed: {
-      playerLife() {
-        return this.getPlayerById('player').life
+      youLife() {
+        return this.getPlayerById('you').life
       },
       monsterLife() {
         return this.getPlayerById('monster').life
       }
     },
     watch: {
-      playerLife(newValue) {
-        if( newValue <= 0 ) {
-          this.getPlayerById('player').life = 0;
-          this.showMessage = true;
-          this.isPlaying = false;
+      youLife(value) {
+        if(!value) {
+          this.result.isShow = true;
+          this.result.message = 'O monstro comeu seu cu!';
         }
       },
-      monsterLife(newValue) {
-        if( newValue <= 0 ) {
-          this.getPlayerById('monster').life = 0;
-          this.showMessage = true;
-          this.isPlaying = false;
+      monsterLife(value) {
+        if(!value) {
+          this.result.isShow = true;
+          this.result.message = 'Você botou o monstro pra mamar!';
         }
       }
     }
