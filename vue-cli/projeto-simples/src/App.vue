f2cb84a274d5411cc874a5ddfff2432a79c9b286
Ricardo Alves <ricardospalves@gmail.com>
2019-02-06 00:19:42 -0200

    Começando a usar o vue-cli

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>
+      {{ title }}
+    </h1>
+
+    <button @click="title += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Teste `data` usando VueJS!!!'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    background-color: coral;
+  }
+</style>
