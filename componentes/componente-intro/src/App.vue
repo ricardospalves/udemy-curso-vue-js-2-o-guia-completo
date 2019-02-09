7c49c6c05cf77fbafc644a6d8d1f75beaf80f25c
Ricardo Alves <ricardospalves@gmail.com>
2019-02-09 00:45:40 -0200

    Criando componetes a partir do vue-cli

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>
+      Contadores
+    </h1>
+
+    <p>{{ contador }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        contador: 0
+      }
+    }
+  }
+</script>
