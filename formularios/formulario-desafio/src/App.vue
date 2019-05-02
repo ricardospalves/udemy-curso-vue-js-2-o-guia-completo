c120f8387d800874d457eb1b5bac7c0539db2ef2
Ricardo Alves <ricardospalves@gmail.com>
2019-05-01 21:06:38 -0300

    Resolução do desafio

--- formularios/formulario-desafio/src/App.vue
+++ formularios/formulario-desafio/src/App.vue
@@ -1,38 +1,86 @@
 <template>
 	<div id="app">
 		<h1>Formulário Desafio</h1>
-		<div class="conteudo">
-			<form class="painel">
-				<div class="cabecalho">Formulário</div>
-				<!-- Exercicio 01 -->
-				<!-- Criar uma formulário de registro -->
-				<!-- Nome completo (Nome e Sobrenome) -->
-				<!-- Email -->
-				<!-- Senha -->
-				<!-- Armazenar Dados? (Sim/Não) -->
 
-				<!-- Exercicio 02 -->
-				<!-- Só mostrar o fomulário de não tiver sido submetido -->
-				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
-				
-				<!-- Exercicio 03 -->
-				<!-- Crie um componente personalizado NomeCompleto -->
-				<!-- Esse componente deve receber Nome e Sobrenome -->
+		<div class="conteudo">
+			<form class="painel" v-if="!isSent">
+				<div class="cabecalho">Formulário</div>
+
+        <NomeCompleto v-model="user.fullName"/>
+
+        <Rotulo nome="E-mail">
+          <input type="email" v-model="user.email">
+        </Rotulo>
+
+        <Rotulo nome="Senha">
+          <input type="password" v-model="user.password">
+        </Rotulo>
+
+        <Rotulo nome="Armazenar Dados?">
+          <input type="checkbox" v-model="user.storage">
+        </Rotulo>
+
+        <button @click.prevent="submit">
+          Enviar
+        </button>
 			</form>
-			<div class="painel">
+
+			<div class="painel" v-else>
 				<div class="cabecalho">Resultado</div>
 
+        <Rotulo nome="Nome">
+          <span>{{ user.fullName.name }}</span>
+        </Rotulo>
+
+        <Rotulo nome="Sobrenome">
+          <span>{{ user.fullName.surname }}</span>
+        </Rotulo>
+
+        <Rotulo nome="E-mail">
+          <span>{{ user.email }}</span>
+        </Rotulo>
+
+        <Rotulo nome="Senha">
+          <span>{{ user.password }}</span>
+        </Rotulo>
+
+        <Rotulo nome="Armazenar Dados?">
+          <span>{{ user.storage }}</span>
+        </Rotulo>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import NomeCompleto from './components/NomeCompleto.vue'
 import Rotulo from './components/Rotulo.vue'
 
 export default {
 	name: 'app',
-	components: { Rotulo }
+	components: {
+    Rotulo,
+    NomeCompleto
+  },
+  methods: {
+    submit() {
+      this.isSent = true
+    }
+  },
+  data() {
+    return {
+      isSent: false,
+      user: {
+        fullName: {
+          name: '',
+          surname: '',
+        },
+        email: '',
+        password: '',
+        storage: true
+      }
+    }
+  }
 }
 </script>
 
---
+++ formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <Rotulo nome="Nome">
+      <input type="text"
+        :value="value.name"
+        @input="changeName"
+      >
+    </Rotulo>
+
+    <Rotulo nome="Sobrenome">
+      <input type="text"
+        :value="value.surname"
+        @input="changeSurname"
+      >
+    </Rotulo>
+  </div>
+</template>
+
+<script>
+import Rotulo from './Rotulo.vue'
+
+export default {
+  props: {
+    value: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    Rotulo
+  },
+  methods: {
+    changeName(event) {
+      this.$emit('input', {
+        name: event.target.value,
+        surname: this.value.surname
+      })
+    },
+    changeSurname(event) {
+      this.$emit('input', {
+        name: this.value.name,
+        surname: event.target.value
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
