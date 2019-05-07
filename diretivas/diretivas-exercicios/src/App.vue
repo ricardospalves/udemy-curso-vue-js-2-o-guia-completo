6a4fd2e14b8bc9c586914f6fef7733852b3869de
Ricardo Alves <ricardospalves@gmail.com>
2019-05-06 23:04:09 -0300

    Módulo de diretivas

---
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="app">
+		<h1>Diretivas</h1>
+
+    <p v-text="'Usando a diretiva <strong>v-text</strong>'"></p>
+
+    <hr>
+
+    <p v-html="'Usando a diretiva <strong>v-html</strong>'"></p>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
