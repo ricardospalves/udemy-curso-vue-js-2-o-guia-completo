4370af6436eca5ed3a9ad8ea2e54fec54df19e24
Ricardo Alves <ricardospalves@gmail.com>
2019-06-02 17:08:32 -0300

    Módulo de transições

---
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+
+    <hr>
+
+    <b-button
+      class="mb-4"
+      variant="primary"
+      @click="isShow = !isShow"
+    >
+      Alternar alerta
+    </b-button>
+
+    <transition>
+      <b-alert
+        show
+        v-if="isShow"
+      >
+        {{ message }}
+      </b-alert>
+    </transition>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+    return {
+      message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, quisquam!',
+      isShow: false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
+}
+</style>
