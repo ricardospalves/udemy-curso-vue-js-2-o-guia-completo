c1493b56af1508a11625600f61a587bb1a9f5899
Ricardo Alves <ricardospalves@gmail.com>
2019-08-07 22:54:20 -0300

    Navegação via js (programação imperativa)

--- rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <h2>Usuários</h2>
+
+    <br>
+
+    <button sucesso @click="goToHome">Voltar para a página inicial</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    goToHome() {
+      // this.$router.push('/')
+      this.$router.push({
+        path: '/'
+      })
+    }
+  }
 }
 </script>
 
