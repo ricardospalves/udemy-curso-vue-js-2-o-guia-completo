02abd0b5b2ab83155b72446952cfc6df2f88283a
Ricardo Alves <ricardospalves@gmail.com>
2019-09-10 22:35:02 -0300

    Vuex e Two-Way-Binding (v-model)

--- vuex/vuex-exercicios/src/store/store.js
+++ vuex/vuex-exercicios/src/store/store.js
@@ -5,7 +5,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    produtos: []
+    produtos: [],
+    quantidade: 2,
+    preco: 19.99
   },
   getters: {
     valorTotal(state) {
@@ -17,6 +19,12 @@ export default new Vuex.Store({
   mutations: {
     adicionarProduto(state, payload) {
       state.produtos.push(payload)
+    },
+    setQuantidade(state, payload) {
+      state.quantidade = payload
+    },
+    setPreco(state, payload) {
+      state.preco = payload
     }
   },
   actions: {
