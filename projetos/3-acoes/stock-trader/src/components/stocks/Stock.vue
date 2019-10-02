8067942cf5510f44c308958651506879f145fe18
Ricardo Alves <ricardospalves@gmail.com>
2019-10-01 22:25:32 -0300

    Criando o Componente de Ações (Stocks)

--- projetos/3-acoes/stock-trader/src/components/stocks/Stock.vue
+++ projetos/3-acoes/stock-trader/src/components/stocks/Stock.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1>Ação</h1>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>NOME <small>(Preço: PRECO)</small></strong>
+      </v-card-title>
+    </v-card>
+
+    <v-card>
+      <v-container class="fill-height">
+        <v-text-field label="Quantidae" type="number"/>
+
+        <v-btn class="green darken-3 white--text">
+          Comprar
+        </v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
