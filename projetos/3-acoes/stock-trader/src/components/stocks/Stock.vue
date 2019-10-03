1983aee8a026e906aa88e9fd7e1c7448527faf7d
Ricardo Alves <ricardospalves@gmail.com>
2019-10-03 00:08:39 -0300

    Adicionando o Botão Comprar

--- projetos/3-acoes/stock-trader/src/components/stocks/Stock.vue
+++ projetos/3-acoes/stock-trader/src/components/stocks/Stock.vue
@@ -2,15 +2,23 @@
   <v-flex class="pr-3 pb-3" xs12 md6 lg4>
     <v-card class="green darken-3 white--text">
       <v-card-title class="headline">
-        <strong>NOME <small>(Preço: PRECO)</small></strong>
+        <strong>{{ stock.name }} <small>(Preço: {{ stock.price }})</small></strong>
       </v-card-title>
     </v-card>
 
     <v-card>
       <v-container class="fill-height">
-        <v-text-field label="Quantidae" type="number"/>
+        <v-text-field
+          label="Quantidae"
+          type="number"
+          v-model.number="quantity"
+        />
 
-        <v-btn class="green darken-3 white--text">
+        <v-btn
+          class="green darken-3 white--text"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          @click="buyStock"
+        >
           Comprar
         </v-btn>
       </v-container>
@@ -20,7 +28,27 @@
 
 <script>
 export default {
+  props: [
+    'stock'
+  ],
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
 
+      console.log(order)
+
+      this.quantity = 0
+    }
+  }
 }
 </script>
 
