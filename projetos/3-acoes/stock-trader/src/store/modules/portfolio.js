5b7b792c1d5fcabc21304ef7f75344eb64970bc7
Ricardo Alves <ricardospalves@gmail.com>
2019-10-08 21:14:49 -0300

    Adicionando o Módulo Portfolio ao Vuex

---
+++ projetos/3-acoes/stock-trader/src/store/modules/portfolio.js
@@ -0,0 +1,59 @@
+export default {
+  state: {
+    funds: 1000,
+    stocks: []
+  },
+  mutations: {
+    buyStock(state, { stockId, quantity, stockPrice }) {
+      const record = state.stocks.find(element => element.id === stockId)
+
+      if(record) {
+        record.quantity += quantity
+      }
+
+      else {
+        state.stocks.push({
+          id: stockId,
+          quantity
+        })
+      }
+
+      state.funds -= stockPrice * quantity
+    },
+    sellStocks(state, { stockId, quantity, stockPrice }) {
+      const record = state.stocks.find(element => element.id === stockId)
+
+      if(record.quantity > quantity) {
+        record.quantity -= quantity
+      }
+
+      else {
+        state.stocks.splice(state.stocks.indexOf(record), 1)
+      }
+
+      state.funds += stockPrice * quantity
+    },
+    actions: {
+      sellStock({ commit }, order) {
+        commit('sellStock', order)
+      }
+    },
+    getters: {
+      stockPortfolio(state, getters) {
+        return state.stocks.map(stock => {
+          const { name, price } = getters.stocks.find(element => element.id === stock.id)
+
+          return {
+            id: stock.id,
+            quantity: stock.quantity,
+            name,
+            price
+          }
+        })
+      },
+      funds(state) {
+        return state.funds
+      }
+    }
+  }
+}
