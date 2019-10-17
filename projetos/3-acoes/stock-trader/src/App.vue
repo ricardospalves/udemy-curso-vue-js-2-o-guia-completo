12f1762cdb7330a2fac521d46ec97182a2078743
Ricardo Alves <ricardospalves@gmail.com>
2019-10-16 22:36:19 -0300

    Animando a Transição de Rotas

--- projetos/3-acoes/stock-trader/src/App.vue
+++ projetos/3-acoes/stock-trader/src/App.vue
@@ -1,10 +1,14 @@
 <template>
 	<v-app>
     <Header/>
-
     <v-content>
       <v-container>
-        <router-view/>
+        <transition
+          name="slide"
+          mode="out-in"
+        >
+          <router-view/>
+        </transition>
       </v-container>
     </v-content>
   </v-app>
@@ -24,6 +28,24 @@ export default {
 </script>
 
 <style>
+  @keyframes slide {
+    from {
+      transform: translateY(-30px);
+      opacity: 0;
+    }
 
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  .slide-enter-active {
+    animation: slide .2s ease;
+  }
+
+  .slide-leave-active {
+    animation: slide .2s ease reverse;
+  }
 </style>
 
